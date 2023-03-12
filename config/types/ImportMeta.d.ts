5c419987d87244722cfff6fa47a3eac63a39cb7e
Truong <anhchangvt1994@gmail.com>
2023-03-13 01:25:41 +0700

    update(other): :jigsaw: update package version
    update package version

--- config/types/ImportMeta.d.ts
+++ config/types/ImportMeta.d.ts
@@ -1,32 +1,32 @@
 interface ImportMeta {
-	env: Env
+    env: Env;
 }
 
 interface Env {
-	PORT: number
-	IO_PORT: number
-	LOCAL_ADDRESS: string
-	LOCAL_HOST: string
-	IPV4_ADDRESS: string
-	IPV4_HOST: string
-	IO_HOST: string
-	ROUTER_BASE_PATH: string
-	ROUTER_HOME_NAME: string
-	ROUTER_HOME_PATH: string
-	ROUTER_CONTENT_NAME: string
-	ROUTER_CONTENT_PATH: string
-	ROUTER_CONTENT_COMMENT_NAME: string
-	ROUTER_CONTENT_COMMENT_PATH: string
-	ROUTER_COMMENT_NAME: string
-	ROUTER_COMMENT_PATH: string
-	ROUTER_LOGIN_NAME: string
-	ROUTER_LOGIN_PATH: string
-	ROUTER_NOT_FOUND_NAME: string
-	ROUTER_NOT_FOUND_PATH: string
-	GENERAL_GREETING: string
-	TEST_TEST_TEST: TestTestTest
+    PORT:                        number;
+    IO_PORT:                     number;
+    LOCAL_ADDRESS:               string;
+    LOCAL_HOST:                  string;
+    IPV4_ADDRESS:                string;
+    IPV4_HOST:                   string;
+    IO_HOST:                     string;
+    ROUTER_BASE_PATH:            string;
+    ROUTER_HOME_NAME:            string;
+    ROUTER_HOME_PATH:            string;
+    ROUTER_CONTENT_NAME:         string;
+    ROUTER_CONTENT_PATH:         string;
+    ROUTER_CONTENT_COMMENT_NAME: string;
+    ROUTER_CONTENT_COMMENT_PATH: string;
+    ROUTER_COMMENT_NAME:         string;
+    ROUTER_COMMENT_PATH:         string;
+    ROUTER_LOGIN_NAME:           string;
+    ROUTER_LOGIN_PATH:           string;
+    ROUTER_NOT_FOUND_NAME:       string;
+    ROUTER_NOT_FOUND_PATH:       string;
+    GENERAL_GREETING:            string;
+    TEST_TEST_TEST:              TestTestTest;
 }
 
 interface TestTestTest {
-	name: string
+    name: string;
 }
